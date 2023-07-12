c1d74cf0db8e280fc13963af5590b532f047b719
Matt Meyers <matt@mattmeyers.dev>
2023-07-12 08:20:08 -0400

    Add additional nvim keybinds

--- nvim/lua/main/remap.lua
+++ nvim/lua/main/remap.lua
@@ -1,3 +1,12 @@
 vim.keymap.set("n", "<leader>ft", vim.cmd.Ex)
 
 vim.keymap.set("n", "Q", "<nop>")
+
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+
+vim.keymap.set("n", "<leader><leader>", vim.lsp.buf.format)
